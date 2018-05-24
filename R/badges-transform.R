eaef3ca4923fa24dcc4a7b7a8e4d45279e0532fb
petzi53 <peter_baumgartner@mac.com>
2018-05-24 14:43:57 +0200

    introduction to SO

--- R/badges-transform.R
+++ R/badges-transform.R
@@ -63,11 +63,29 @@ origin <- c(
 badges <- cbind(badges, origin)
 based_on <- c(
         "User",           # 1
-        "Community",      # 2
+        "Peer",           # 2
         "Event",          # 3
         "Dead"            # 4
 )
 badges$origin <- factor(badges$origin, levels = c(1:4), labels = based_on)
+
+
+# 4. Mark same action necessary for badges, but with different degrees of difficulty
+
+alike <- c(
+    "XX","XX","22","20","XX","XX","13","22","09","XX",
+    "16","XX","XX","10","17","09","14","19","XX","01",
+    "18","16","XX","XX","XX","XX","19","XX","XX","11",
+    "12","20","05","04","11","02","XX","06","03","06",
+    "03","XX","05","XX","01","XX","12","16","12","07",
+    "06","03","XX","04","XX","15","XX","04","XX","13",
+    "XX","XX","22","10","14","05","21","XX","18","07",
+    "XX","XX","17","01","XX","02","18","19","XX","XX",
+    "XX","XX","21","15","XX","XX","08","XX","08","XX",
+    "XX"
+)
+badges <- cbind(badges, alike)
+
 
 badges <- badges[, c(
         "badge_id",
@@ -76,7 +94,34 @@ badges <- badges[, c(
         "rank",
         "activity",
         "origin",
+        "alike",
         "description")]
 
+# 5. Clean up manually long text in column "description"
+
+badges[1,8] <- "First bounty you manually award on another person's question"
+badges[5,8] <- "Complete 'About Me' section of user profile"
+badges[7,8] <- "Voted 10 times, added 3 posts score > 0, and visited the site on 3 separate days during the private bet"
+badges[9,8] <- "Visit an election during any phase of an active election and have enough reputation to cast a vote"
+badges[16,8] <- "Vote for a candidate in the final phase of an election"
+badges[17,8] <- "10 posts with score of 2 on meta (MSO)"
+badges[21,8] <- "Complete at least one review task. This badge is awarded once per review type"
+badges[26,8] <- "Earned at least one badge for contributing to Stack Overflow Documentation"
+badges[30,8] <- "Visit the site each day for 30 consecutive days. (Days are counted in UTC.)"
+badges[33,8] <- "Edit and answer 1 question (both actions within 12 hours, answer score > 0)"
+badges[35,8] <- "Visit the site each day for 100 consecutive days. (Days are counted in UTC.)"
+badges[43,8] <- "Edit and answer 500 questions (both actions within 12 hours, answer score > 0)"
+badges[44,8] <- "Read the entire tour page"
+badges[46,8] <- "First bounty you offer on another person's question"
+badges[49,8] <- "Earn at least 200 reputation (the daily maximum) in a single day"
+badges[53,8] <- "Met a Stack Overflow employee at an event where Stack Overflow was an organizer or participant with 50 or more attendees"
+badges[56,8] <- "Post 10 messages in chat starred by 10 different users"
+badges[60,8] <- "Followed the Area 51 proposal for this site before it entered the commitment phase"
+badges[65,8] <- "One post with score of 2 on met"
+badges[66,8] <- "Edit and answer 50 questions (both actions within 12 hours, answer score > 0)"
+badges[69,8] <- "Complete at least 250 review tasks. This badge is awarded once per review type"
+badges[77,8] <- "Complete at least 1,000 review tasks. This badge is awarded once per review type"
+badges[82,8] <- "First approved tag synonym"
+badges[84,8] <- "Post 10 messages, with 1 or more starred, in chat"
 
 saveRDS(badges, file = "data-processed/badges.rds")
